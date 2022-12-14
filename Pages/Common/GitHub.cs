3f5b3c8bb3514d6eeb514bc711e69e6f493b1f36
Jonas Lara <adrianlr2515@outlook.com>
2022-12-14 12:57:36 -0600

    GitHub

---
+++ Pages/Common/GitHub.cs
@@ -0,0 +1,19 @@
+﻿// #1
+using Octokit;
+
+// #2
+var username = "Jonas-Lara";
+
+// #3
+var github = new GitHubClient(new ProductHeaderValue("MyAmazingApp"));
+var user = await github.User.Get(username);
+var repos = await github.Repository.GetAllForUser(user.Login);
+
+// #4
+foreach (var repo in repos.OrderByDescending(x => x.StargazersCount))
+{
+    Console.WriteLine(Environment.NewLine);
+    Console.WriteLine("Name: {0}", repo.Name);
+    Console.WriteLine("URL: {0}", repo.HtmlUrl);
+    Console.WriteLine("Stars: {0}", repo.StargazersCount);
+}
